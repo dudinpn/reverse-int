71e52dc33dcb01c555354197565ad8dcf5384f1a
Pasha <dudinpn@mail.ru>
2022-02-02 19:23:35 +0300

    init: completed reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+    var nStr = n.toString();
+    let arr = nStr.split("");
+    let arrRev = arr.reverse();
+    let answer = arrRev.join("");
+
+    return parseInt(answer);
   
-}
+};
